80e6fbf6912ed0aac2a21f1b98e7abf231391ce4
MetelevNikita <Kyle.B@mail.ru>
2024-03-29 10:35:41 +0500

    DEPLOY v1.2

--- src/components/pages/jsx/team.jsx
+++ src/components/pages/jsx/team.jsx
@@ -18,6 +18,10 @@ import TeamCard from '../../../UI/teamCard'
 //
 
 import ScrollCarousel from 'scroll-carousel-react'
+
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
+
 import { useEffect } from 'react'
 
 
@@ -42,13 +46,13 @@ const Team = () => {
   return(
 
       <Row>
-        <Col className='col-12 mt-5 mb-5'>
+        <Col md={12} className='mt-5 mb-5'>
 
         <div className="team-title">наша команда</div>
         <div className="team-subtitle">наши специалисты</div>
 
 
-        <ScrollCarousel autoplay autoplaySpeed={5}>
+        <ScrollCarousel autoplay autoplaySpeed={4}>
           {teamPerson.map((card, index) => {
             console.log(teamPerson)
             return <Col key={index}><TeamCard img={card.img} name={card.name} profession={card.profession}></TeamCard></Col>
